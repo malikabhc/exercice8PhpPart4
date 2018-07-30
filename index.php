46c9a0948eca4329a7a669245e5dea940c56ce7f
Malikabhc <malika.bouhcine@gmail.com>
2018-07-30 15:05:28 +0200

    exercice8-4

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 7/4</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        function addition($number1, $number2, $number3){
+          return $number1 + $number2 + $number3;
+        }
+        echo addition(10, 5, 25);
+      ?>
+    </p>
+  </body>
+</html>
