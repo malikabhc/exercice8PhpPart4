6fb1b66b74c2b9f25c5e932419cd1af2a80b936b
Malikabhc <malika.bouhcine@gmail.com>
2018-08-01 11:01:04 +0200

    correction

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
+<?php
+function calculate($number1 = 10, $number2 = 5, $number3 = 25){
+  return $number1 + $number2 + $number3;
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
     <meta charset="utf-8" />
-    <title>Exercice 7/4</title>
+    <title>Exercice 8/4</title>
   </head>
   <body>
     <p>
-      <?php
-        function addition($number1, $number2, $number3){
-          return $number1 + $number2 + $number3;
-        }
-        echo addition(10, 5, 25);
-      ?>
+      <?= calculate(); // Si on met des paramètres ils écrasent les paramètres par défaut ?> 
     </p>
   </body>
 </html>
